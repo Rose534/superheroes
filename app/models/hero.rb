df23ef97ce086186684f692360e2e4883fd33919
rose534 <wambuirose534@gmail.com>
2023-03-27 15:13:02 +0300

    readme

--- app/models/hero.rb
+++ app/models/hero.rb
@@ -2,5 +2,5 @@ class Hero < ApplicationRecord
     has_many :hero_powers
     has_many :powers, through: :hero_powers
 
-    # validates :strength, inclusion: { in: %w(Strong Weak Average)}
+    
 end
